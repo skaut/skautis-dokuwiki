cf57a8823710aa7d88f932918cc4564bdab3b211
Jan Staněk <stanej16@fit.cvut.cz>
2017-11-19 14:05:54 +0100

    plugin settings labels

---
+++ lang/cs/settings.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['skautis_app_id'] = "SkautIS AppID";
+$lang['skautis_test_mode'] = "Použít testovací skautIS";
+$lang['skautis_allowed_add_user'] = "Povolit přihlášení novým uživatelům";
+?>
---
+++ lang/en/settings.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['skautis_app_id'] = "SkautIS AppID";
+$lang['skautis_test_mode'] = "Use test skautIS";
+$lang['skautis_allowed_add_user'] = "Allow new user login";
+?>
