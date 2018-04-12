a57e8f1a4a07fb66885a0da0d0f6884bf97f4692
Jan Staněk <jan@jstanek.cz>
2018-04-12 23:18:56 +0200

    úprava generování username, oprava chyby, refactoring

--- action.php
+++ action.php
@@ -1,22 +1,24 @@
 <?php
 
+// must be run within DokuWiki
 if (!defined('DOKU_INC')) die();
 
 require_once __DIR__ . '/vendor/autoload.php';
 
 
-class action_plugin_authskautis extends DokuWiki_Action_Plugin {
-
+class action_plugin_authskautis extends DokuWiki_Action_Plugin
+{
     /**
      * Registers the event handlers.
      */
     function register(Doku_Event_Handler $controller)
     {
-        $controller->register_hook('HTML_LOGINFORM_OUTPUT', 'BEFORE',  $this, 'hook_html_loginform_output', []);
+        $controller->register_hook('HTML_LOGINFORM_OUTPUT', 'BEFORE', $this, 'hook_html_loginform_output', []);
         $controller->register_hook('HTML_UPDATEPROFILEFORM_OUTPUT', 'BEFORE', $this, 'hook_updateprofileform_output', []);
     }
 
-    function hook_updateprofileform_output(&$event, $param) {
+    function hook_updateprofileform_output(&$event, $param)
+    {
         global $USERINFO;
 
         if ($USERINFO['is_skautis']) {
@@ -28,29 +30,31 @@ class action_plugin_authskautis extends DokuWiki_Action_Plugin {
             $elem['disabled'] = 'disabled';
             $event->data->replaceElement(3, $elem);
 
-            $event->data->replaceElement(10, null);
-            $event->data->replaceElement(9, null);
-            $event->data->replaceElement(8, null);
-            $event->data->replaceElement(7, null);
-            $event->data->replaceElement(6, null);
-            $event->data->replaceElement(5, null);
-            $event->data->replaceElement(4, null);
+            $event->data->replaceElement(10, NULL);
+            $event->data->replaceElement(9, NULL);
+            $event->data->replaceElement(8, NULL);
+            $event->data->replaceElement(7, NULL);
+            $event->data->replaceElement(6, NULL);
+            $event->data->replaceElement(5, NULL);
+            $event->data->replaceElement(4, NULL);
         }
     }
 
     /**
      * Handles the login form rendering.
      */
-    function hook_html_loginform_output(&$event, $param) {
+    function hook_html_loginform_output(&$event, $param)
+    {
         $skautIsAppId = $this->getConf('skautis_app_id');
         $skautIsTestmode = $this->getConf('skautis_test_mode');
 
-        if($skautIsAppId!=''){
+        if ($skautIsAppId != '') {
+            $skautIs = Skautis\Skautis::getInstance($skautIsAppId, $skautIsTestmode);
             $loginUrl = $skautIs->getLoginUrl();
             $buttonText = $this->getLang('enter_skautis');
             echo "<a href='$loginUrl' class='login-button' title='$buttonText'><span class='login-button-logo'>&#x00ac;</span> $buttonText</a>";
-
         }
     }
 }
+
 ?>
